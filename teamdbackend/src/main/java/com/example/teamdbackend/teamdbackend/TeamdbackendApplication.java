5733b339b02bca33fba73a0353e9fd3e3f57fe55
Allada.Joshua <147649070+AlladaJoshua@users.noreply.github.com>
2024-01-16 14:40:23 +0800

    first commit

---
+++ teamdbackend/src/main/java/com/example/teamdbackend/teamdbackend/TeamdbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamdbackend.teamdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamdbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamdbackendApplication.class, args);
+	}
+
+}
